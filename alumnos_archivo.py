3da587c8394e733a2c8ce576a128e176088ce66a
velariscourt <luuaedlp@gmail.com>
2022-11-22 19:17:25 -0300

    primero 22/11

--- alumnos_archivo.py
+++ alumnos_archivo.py
@@ -41,21 +41,28 @@ Entry(root, width=20, textvariable=matVar).grid(padx=5, row=2, column=1)
 notaVar = StringVar()
 Entry(root, width=20, textvariable=notaVar).grid(padx=5, row=3, column=1)
 
+
+def normalizar(s):
+    a,b = 'áéíóúü','aeiouu'
+    trans = str.maketrans(a,b)
+    return s.translate(trans)
+
 def agregar():
     global listado
     datos = {}
 
     nomVar.set(nomVar.get().lower())
     apeVar.set(apeVar.get().lower())
+    matVar.set(matVar.get().lower())
 
-    datos[matVar.get()]=notaVar.get()
+    datos[normalizar(matVar.get())]=notaVar.get()
     
     if not apeVar.get()+nomVar.get() in listado.keys():
         listado[apeVar.get()+nomVar.get()]={}
-        listado[apeVar.get()+nomVar.get()]["Nombre"]=nomVar.get()
-        listado[apeVar.get()+nomVar.get()]["Apellido"]=apeVar.get()
-        listado[apeVar.get()+nomVar.get()]['materias']=[]
-    listado[apeVar.get()+nomVar.get()]['materias'].append(datos)
+        listado[apeVar.get()+nomVar.get()]["Nombre"]=normalizar(nomVar.get())
+        listado[apeVar.get()+nomVar.get()]["Apellido"]=normalizar(apeVar.get())
+        listado[apeVar.get()+nomVar.get()]["Materias"]=[]
+    listado[apeVar.get()+nomVar.get()]["Materias"].append(datos)
     
     print(f"Agregar {apeVar.get()}")
     print (listado)
@@ -72,22 +79,27 @@ def salir():
 
 def ventana_sec(lista):
     ventana=Tk()
-    ventana.title(f"{apeVar.get().upper()} {nomVar.get().upper()}")
+    ventana.title(f"{apeVar.get().upper()}, {nomVar.get().upper()}")
     
-    listbox = Listbox(ventana, height = 20,
+    listbox = Listbox(ventana, justify = "center",
                   width = 100,
                   bg = "grey",
-                  activestyle = 'dotbox',
+                  activestyle = "dotbox",
                   font = "Arial",
-                  fg = "black")
-    
+                  fg = "black"
+                  height = 20,
+                  upper = "listbox"
+                  )                       
+
     for a in lista:
         for k, v in a.items():
             listbox.insert(END, f"{k}: {v}")
 
     listbox.pack()
 
-# Agregar mayúsculas, centrar y jerarquización del nombre y apellido (ambos en mayus)
+# Solucionar mayúsculas y minúsculas. Como lo introduces, es como se muestra en la lista; se tienen que mostrar en mayúscula en la lista sin importar cómo se hayan agregado
+# Centrar y jerarquización del nombre y apellido (ambos en mayus)
+# Agregar enter
 
 def consultar():
     if apeVar.get()=="" or nomVar.get()=="":
@@ -97,9 +109,7 @@ def consultar():
         if not apeVar.get()+nomVar.get() in listado.keys():
             messagebox.showerror("ERROR", "El alumno no se encuentra en el archivo")
         else:
-            ventana_sec(listado[apeVar.get()+nomVar.get()]["materias"])
-
-
+            ventana_sec(listado[apeVar.get()+nomVar.get()]["Materias"])
 
 Button(root, text="Agregar", width=20, command=agregar).grid(padx=10, pady=10, row=4, column=0)
 Button(root, text="Salir", width=20, command=salir).grid(padx=10, pady=10, row=4, column=1)
